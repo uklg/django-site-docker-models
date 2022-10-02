d8480a84f1076dc520c9bc8556a860c16aad2254
rmu <rmu@live.co.uk>
2022-10-02 17:42:28 +0100

    update the item

--- pages/models.py
+++ pages/models.py
@@ -1,35 +1,15 @@
 from django.db import models
 
-class Customer(models.Model):
-    name = models.CharField(max_length=30)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 class Town(models.Model):
     name = models.CharField(max_length=25)
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=30)
+    town = models.ForeignKey(Town,on_delete=models.CASCADE)
+
+
 
 
 
