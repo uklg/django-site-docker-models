adb6a2127c6ee21bfaa849c54e93be2c5b440f5d
rmu <rmu@live.co.uk>
2022-10-02 17:00:22 +0100

    add model and tie it into admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from pages.models import Customer
+
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Customer,CustomerAdmin)
+    
