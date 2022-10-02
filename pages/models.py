4aee607778a23d8db4ab849e9ab8eefeb3c307d4
Richard Murdoch <rmu@live.co.uk>
2022-10-02 23:15:35 +0100

    working

--- pages/models.py
+++ pages/models.py
@@ -3,12 +3,15 @@ from django.db import models
 
 class Town(models.Model):
     name = models.CharField(max_length=25)
+    def __str__(self):
+        return self.name[:50]
 
 
 class Customer(models.Model):
     name = models.CharField(max_length=30)
     town = models.ForeignKey(Town,on_delete=models.CASCADE)
-
+    def __str__(self):
+        return self.name[:50]
 
 
 
