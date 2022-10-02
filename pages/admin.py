1c4634158c504f7abfc1e20537c5dd5329b401e9
rmu <rmu@live.co.uk>
2022-10-02 17:15:35 +0100

    add in towns

--- pages/admin.py
+++ pages/admin.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 
 # Register your models here.
 
-from pages.models import Customer
+from pages.models import Customer, Town
 
 class CustomerAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Customer,CustomerAdmin)
-    
+
+
+
+
+class TownAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Town,TownAdmin)
--- pages/models.py
+++ pages/models.py
@@ -2,3 +2,36 @@ from django.db import models
 
 class Customer(models.Model):
     name = models.CharField(max_length=30)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Town(models.Model):
+    name = models.CharField(max_length=25)
+
+
+
+
+    
